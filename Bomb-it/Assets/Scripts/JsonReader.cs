d50a16604ca25ef4426fddba3c6edf72066ecbfc
tonymtz <atony.mtz@gmail.com>
2017-02-05 14:37:11 -0700

    world serializer

---
+++ Bomb-it/Assets/Scripts/JsonReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public static class JsonReader {
+    public static Worldx Read() {
+        String worldFile = File.ReadAllText("Assets/map01.json");
+
+        Worldx myWorld = JsonUtility.FromJson<Worldx>(worldFile);
+
+        return myWorld;
+    }
+}
---
+++ Bomb-it/Assets/Scripts/Worldx.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Worldx {
+    public List<int> blocks;
+    public int rowSize;
+}
